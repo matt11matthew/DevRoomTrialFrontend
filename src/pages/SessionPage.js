80d95ffcf777d17caebfaa6727f5024b4a24f959
matt11matthew <matt11matthew@gmail.com>
2025-01-19 10:46:51 -0500

    1

---
+++ src/pages/SessionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+
+function SessionPage() {
+    return (
+        <div className="SessionPage">
+            <PageTitle title="Session Management" />
+        </div>
+    );
+}
+
+export default SessionPage;
